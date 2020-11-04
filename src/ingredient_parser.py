61411a47624d00c92bea5227bdd3de05addc22d4
Jack <jackmleitch@gmail.com>
2020-11-04 14:41:25 -0700

    Scraped more data + saved to df_recipes

--- src/ingredient_parser.py
+++ src/ingredient_parser.py
@@ -54,7 +54,10 @@ def ingredient_parser(ingreds):
     measures = ['teaspoon', 't', 'tsp.', 'tablespoon', 'T', 'tbl.', 'tb', 'tbsp.', 'fluid ounce', 'fl oz', 'gill', 'cup', 'c', 'pint', 'p', 'pt', 'fl pt', 'quart', 'q', 'qt', 'fl qt', 'gallon', 'g', 'gal', 'ml', 'milliliter', 'millilitre', 'cc', 'mL', 'l', 'liter', 'litre', 'L', 'dl', 'deciliter', 'decilitre', 'dL', 'bulb', 'level', 'heaped', 'rounded', 'whole', 'pinch', 'medium', 'slice', 'pound', 'lb', '#', 'ounce', 'oz', 'mg', 'milligram', 'milligramme', 'g', 'gram', 'gramme', 'kg', 'kilogram', 'kilogramme', 'x', 'of', 'mm', 'millimetre', 'millimeter', 'cm', 'centimeter', 'centimetre', 'm', 'meter', 'metre', 'inch', 'in', 'milli', 'centi', 'deci', 'hecto', 'kilo']
     words_to_remove = ['fresh', 'oil', 'a', 'red', 'bunch', 'and', 'clove', 'or', 'leaf', 'chilli', 'large', 'extra', 'sprig', 'ground', 'handful', 'free', 'small', 'pepper', 'virgin', 'range', 'from', 'dried', 'sustainable', 'black', 'peeled', 'higher', 'welfare', 'seed', 'for', 'finely', 'freshly', 'sea', 'quality', 'white', 'ripe', 'few', 'piece', 'source', 'to', 'organic', 'flat', 'smoked', 'ginger', 'sliced', 'green', 'picked', 'the', 'stick', 'plain', 'plus', 'mixed', 'mint', 'bay', 'basil', 'your', 'cumin', 'optional', 'fennel', 'serve', 'mustard', 'unsalted', 'baby', 'paprika', 'fat', 'ask', 'natural', 'skin', 'roughly', 'into', 'such', 'cut', 'good', 'brown', 'grated', 'trimmed', 'oregano', 'powder', 'yellow', 'dusting', 'knob', 'frozen', 'on', 'deseeded', 'low', 'runny', 'balsamic', 'cooked', 'streaky', 'nutmeg', 'nut', 'sage', 'rasher', 'leek', 'zest', 'pin', 'groundnut', 'breadcrumb', 'turmeric', 'halved', 'grating', 'stalk', 'light', 'tinned', 'dry', 'soft', 'rocket', 'minced', 'bone', 'colour', 'washed', 'skinless', 'leftover', 'splash', 'removed', 'dijon', 'thick', 'big', 'hot', 'drained', 'sized', 'chestnut', 'watercress', 'fishmonger', 'english', 'dill', 'caper', 'raw', 'worcestershire', 'flake', 'cider', 'cayenne', 'tbsp', 'leg', 'pine', 'wild', 'if', 'fine', 'herb', 'almond', 'shoulder', 'cube', 'dressing', 'with', 'chunk', 'spice', 'thumb', 'garam', 'new', 'little', 'punnet', 'peppercorn', 'shelled', 'saffron', 'other']
     # The ingredient list is now a string so we need to turn it back into a list. We use ast.literal_eval
-    ingredients = ast.literal_eval(ingreds)
+    if isinstance(ingreds, list):
+        ingredients = ingreds
+    else:
+        ingredients = ast.literal_eval(ingreds)
     # We first get rid of all the punctuation. We make use of str.maketrans. It takes three input 
     # arguments 'x', 'y', 'z'. 'x' and 'y' must be equal-length strings and characters in 'x'
     # are replaced by characters in 'y'. 'z' is a string (string.punctuation here) where each character
@@ -85,6 +88,7 @@ if __name__ == "__main__":
     recipe_df = pd.read_csv("/Users/Jack/Documents/ML/Projects/Whatscooking-/input/JamieOliver_full.csv")
     recipe_df['ingredients'] = recipe_df['ingredients'].apply(lambda x: ingredient_parser(x))
     df = recipe_df[['recipe_name', 'ingredients']]
+    df = recipe_df.dropna()
     df.to_csv(r"/Users/Jack/Documents/ML/Projects/Whatscooking-/input/df_parsed.csv", index=False)
 
 # Remove useless easy words 
