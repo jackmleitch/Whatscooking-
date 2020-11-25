d8d5838bdfa97fdc658fe1c6f45f05d551aadaa9
Jack <jackmleitch@gmail.com>
2020-11-25 11:35:15 -0500

    ingredient parser updated with more recipes

--- src/ingredient_parser.py
+++ src/ingredient_parser.py
@@ -1,10 +1,11 @@
-# /Users/Jack/Documents/ML/Projects/Whatscooking-/src
+# /Users/Jack/Documents/Projects/Whatscooking-/src
 
 import pandas as pd 
 import nltk
 import string
 import ast
 import re
+import unidecode
 # nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
@@ -12,24 +13,6 @@ from collections import Counter
 
 # Weigths and measures are words that will not add value to the model. I got these standard words from 
 # https://en.wikibooks.org/wiki/Cookbook:Units_of_measurement
-
-# volume = ['teaspoon', 't', 'tsp.', 'tablespoon', 'T', 'tbl.', 'tbs', 'tbsp.', 'fluid ounce', 
-#           'fl oz', 'gill', 'cup', 'c', 'pint', 'p', 'pt', 'fl pt', 'quart', 'q', 'qt', 'fl qt',
-#           'gallon', 'g', 'gal', 'ml', 'milliliter', 'millilitre', 'cc', 'mL', 'l', 'liter', 'litre',
-#           'L', 'dl', 'deciliter', 'decilitre', 'dL', 'bulb', 'level', 'heaped', 'rounded', 'whole',
-#           'pinch', 'medium', 'slice']
-
-# mass = ['pound', 'lb', '#', 'ounce', 'oz', 'mg', 'milligram', 'milligramme', 'g', 'gram', 'gramme',
-#         'kg', 'kilogram', 'kilogramme', 'x', 'of']
-
-# length = ['mm', 'millimetre', 'millimeter', 'cm', 'centimeter', 'centimetre', 'm', 'meter', 'metre',
-#           'inch', 'in']
-
-# prefix = ['milli', 'centi', 'deci', 'hecto', 'kilo']
-
-# words_to_remove = ['fresh', 'oil', 'a', 'red', 'bunch', 'and', 'clove', 'or', 'leaf', 'chilli', 'large', 'extra', 'sprig', 'ground', 'handful', 'free', 'small', 'pepper', 'virgin', 'range', 'from', 'dried', 'sustainable', 'black', 'peeled', 'higher', 'welfare', 'seed', 'for', 'finely', 'freshly', 'sea', 'quality', 'white', 'ripe', 'few', 'piece', 'source', 'to', 'organic', 'flat', 'smoked', 'ginger', 'sliced', 'green', 'picked', 'the', 'stick', 'plain', 'plus', 'mixed', 'mint', 'bay', 'basil', 'your', 'cumin', 'optional', 'fennel', 'serve', 'mustard', 'unsalted', 'baby', 'paprika', 'fat', 'ask', 'natural', 'skin', 'roughly', 'into', 'such', 'cut', 'good', 'brown', 'grated', 'trimmed', 'oregano', 'powder', 'yellow', 'dusting', 'knob', 'frozen', 'on', 'deseeded', 'low', 'runny', 'balsamic', 'cooked', 'streaky', 'nutmeg', 'nut', 'sage', 'rasher', 'leek', 'zest', 'pin', 'groundnut', 'breadcrumb', 'turmeric', 'halved', 'grating', 'stalk', 'light', 'tinned', 'dry', 'soft', 'rocket', 'minced', 'bone', 'colour', 'washed', 'skinless', 'leftover', 'splash', 'removed', 'dijon', 'thick', 'big', 'hot', 'drained', 'sized', 'chestnut', 'watercress', 'fishmonger', 'english', 'dill', 'caper', 'raw', 'worcestershire', 'flake', 'cider', 'cayenne', 'tbsp', 'leg', 'pine', 'wild', 'if', 'fine', 'herb', 'almond', 'shoulder', 'cube', 'dressing', 'with', 'chunk', 'spice', 'thumb', 'garam', 'new', 'little', 'punnet', 'peppercorn', 'shelled', 'saffron', 'other']
-
-# measures = volume + mass + length + prefix 
 
 # # We lemmatize the words to reduce them to their smallest form (lemmas). 
 # lemmatizer = WordNetLemmatizer()
@@ -52,7 +35,7 @@ def ingredient_parser(ingreds):
 
     '''
     measures = ['teaspoon', 't', 'tsp.', 'tablespoon', 'T', 'tbl.', 'tb', 'tbsp.', 'fluid ounce', 'fl oz', 'gill', 'cup', 'c', 'pint', 'p', 'pt', 'fl pt', 'quart', 'q', 'qt', 'fl qt', 'gallon', 'g', 'gal', 'ml', 'milliliter', 'millilitre', 'cc', 'mL', 'l', 'liter', 'litre', 'L', 'dl', 'deciliter', 'decilitre', 'dL', 'bulb', 'level', 'heaped', 'rounded', 'whole', 'pinch', 'medium', 'slice', 'pound', 'lb', '#', 'ounce', 'oz', 'mg', 'milligram', 'milligramme', 'g', 'gram', 'gramme', 'kg', 'kilogram', 'kilogramme', 'x', 'of', 'mm', 'millimetre', 'millimeter', 'cm', 'centimeter', 'centimetre', 'm', 'meter', 'metre', 'inch', 'in', 'milli', 'centi', 'deci', 'hecto', 'kilo']
-    words_to_remove = ['fresh', 'oil', 'a', 'red', 'bunch', 'and', 'clove', 'or', 'leaf', 'chilli', 'large', 'extra', 'sprig', 'ground', 'handful', 'free', 'small', 'pepper', 'virgin', 'range', 'from', 'dried', 'sustainable', 'black', 'peeled', 'higher', 'welfare', 'seed', 'for', 'finely', 'freshly', 'sea', 'quality', 'white', 'ripe', 'few', 'piece', 'source', 'to', 'organic', 'flat', 'smoked', 'ginger', 'sliced', 'green', 'picked', 'the', 'stick', 'plain', 'plus', 'mixed', 'mint', 'bay', 'basil', 'your', 'cumin', 'optional', 'fennel', 'serve', 'mustard', 'unsalted', 'baby', 'paprika', 'fat', 'ask', 'natural', 'skin', 'roughly', 'into', 'such', 'cut', 'good', 'brown', 'grated', 'trimmed', 'oregano', 'powder', 'yellow', 'dusting', 'knob', 'frozen', 'on', 'deseeded', 'low', 'runny', 'balsamic', 'cooked', 'streaky', 'nutmeg', 'nut', 'sage', 'rasher', 'leek', 'zest', 'pin', 'groundnut', 'breadcrumb', 'turmeric', 'halved', 'grating', 'stalk', 'light', 'tinned', 'dry', 'soft', 'rocket', 'minced', 'bone', 'colour', 'washed', 'skinless', 'leftover', 'splash', 'removed', 'dijon', 'thick', 'big', 'hot', 'drained', 'sized', 'chestnut', 'watercress', 'fishmonger', 'english', 'dill', 'caper', 'raw', 'worcestershire', 'flake', 'cider', 'cayenne', 'tbsp', 'leg', 'pine', 'wild', 'if', 'fine', 'herb', 'almond', 'shoulder', 'cube', 'dressing', 'with', 'chunk', 'spice', 'thumb', 'garam', 'new', 'little', 'punnet', 'peppercorn', 'shelled', 'saffron', 'other']
+    words_to_remove = ['fresh', 'oil', 'a', 'red', 'bunch', 'and', 'clove', 'or', 'leaf', 'chilli', 'large', 'extra', 'sprig', 'ground', 'handful', 'free', 'small', 'pepper', 'virgin', 'range', 'from', 'dried', 'sustainable', 'black', 'peeled', 'higher', 'welfare', 'seed', 'for', 'finely', 'freshly', 'sea', 'quality', 'white', 'ripe', 'few', 'piece', 'source', 'to', 'organic', 'flat', 'smoked', 'ginger', 'sliced', 'green', 'picked', 'the', 'stick', 'plain', 'plus', 'mixed', 'mint', 'bay', 'basil', 'your', 'cumin', 'optional', 'fennel', 'serve', 'mustard', 'unsalted', 'baby', 'paprika', 'fat', 'ask', 'natural', 'skin', 'roughly', 'into', 'such', 'cut', 'good', 'brown', 'grated', 'trimmed', 'oregano', 'powder', 'yellow', 'dusting', 'knob', 'frozen', 'on', 'deseeded', 'low', 'runny', 'balsamic', 'cooked', 'streaky', 'nutmeg', 'sage', 'rasher', 'zest', 'pin', 'groundnut', 'breadcrumb', 'turmeric', 'halved', 'grating', 'stalk', 'light', 'tinned', 'dry', 'soft', 'rocket', 'bone', 'colour', 'washed', 'skinless', 'leftover', 'splash', 'removed', 'dijon', 'thick', 'big', 'hot', 'drained', 'sized', 'chestnut', 'watercress', 'fishmonger', 'english', 'dill', 'caper', 'raw', 'worcestershire', 'flake', 'cider', 'cayenne', 'tbsp', 'leg', 'pine', 'wild', 'if', 'fine', 'herb', 'almond', 'shoulder', 'cube', 'dressing', 'with', 'chunk', 'spice', 'thumb', 'garam', 'new', 'little', 'punnet', 'peppercorn', 'shelled', 'saffron', 'other''chopped', 'salt', 'olive', 'taste', 'can', 'sauce', 'water', 'diced', 'package', 'italian', 'shredded', 'divided', 'parsley', 'vinegar', 'all', 'purpose', 'crushed', 'juice', 'more', 'coriander', 'bell', 'needed', 'thinly', 'boneless', 'half', 'thyme', 'cubed', 'cinnamon', 'cilantro', 'jar', 'seasoning', 'rosemary', 'extract', 'sweet', 'baking', 'beaten', 'heavy', 'seeded', 'tin', 'vanilla', 'uncooked', 'crumb', 'style', 'thin', 'nut', 'coarsely', 'spring', 'chili', 'cornstarch', 'strip', 'cardamom', 'rinsed', 'honey', 'cherry', 'root', 'quartered', 'head', 'softened', 'container', 'crumbled', 'frying', 'lean', 'cooking', 'roasted', 'warm', 'whipping', 'thawed', 'corn', 'pitted', 'sun', 'kosher', 'bite', 'toasted', 'lasagna', 'split', 'melted', 'degree', 'lengthwise', 'romano', 'packed', 'pod', 'anchovy', 'rom', 'prepared', 'juiced', 'fluid', 'floret', 'room', 'active', 'seasoned', 'mix', 'deveined', 'lightly', 'anise', 'thai', 'size', 'unsweetened', 'torn', 'wedge', 'sour', 'basmati', 'marinara', 'dark', 'temperature', 'garnish', 'bouillon', 'loaf', 'shell', 'reggiano', 'canola', 'parmigiano', 'round', 'canned', 'ghee', 'crust', 'long', 'broken', 'ketchup', 'bulk', 'cleaned', 'condensed', 'sherry', 'provolone', 'cold', 'soda', 'cottage', 'spray', 'tamarind', 'pecorino', 'shortening', 'part', 'bottle', 'sodium', 'cocoa', 'grain', 'french', 'roast', 'stem', 'link', 'firm', 'asafoetida', 'mild', 'dash', 'boiling']
     # The ingredient list is now a string so we need to turn it back into a list. We use ast.literal_eval
     if isinstance(ingreds, list):
         ingredients = ingreds
@@ -73,6 +56,8 @@ def ingredient_parser(ingreds):
         items = [word for word in items if word.isalpha()]
         # Turn everything to lowercase
         items = [word.lower() for word in items]
+        # remove accents
+        items = [unidecode.unidecode(word) for word in items] #''.join((c for c in unicodedata.normalize('NFD', items) if unicodedata.category(c) != 'Mn'))
         # Lemmatize words so we can compare words to measuring words
         items = [lemmatizer.lemmatize(word) for word in items]
         # Gets rid of measuring words/phrases, e.g. heaped teaspoon
@@ -85,27 +70,27 @@ def ingredient_parser(ingreds):
     return ingred_list
 
 if __name__ == "__main__":
-    recipe_df = pd.read_csv("/Users/Jack/Documents/ML/Projects/Whatscooking-/input/JamieOliver_full.csv")
+    recipe_df = pd.read_csv("/Users/Jack/Documents/Projects/Whatscooking-/input/df_recipes.csv")
     recipe_df['ingredients'] = recipe_df['ingredients'].apply(lambda x: ingredient_parser(x))
     df = recipe_df[['recipe_name', 'ingredients']]
     df = recipe_df.dropna()
-    df.to_csv(r"/Users/Jack/Documents/ML/Projects/Whatscooking-/input/df_parsed.csv", index=False)
 
-# Remove useless easy words 
+    # remove - Allrecipes.com from end of every recipe title 
+    m = df.recipe_name.str.endswith('Recipe - Allrecipes.com')
+    df['recipe_name'].loc[m] = df.recipe_name.loc[m].str[:-23]        
 
-# vocabulary = nltk.FreqDist()
-# for ingredients in recipe_df['ingredients']:
-#     for ingred in ingredients:
-#         temp = ingred.split()
-#         vocabulary.update(temp)
+    df.to_csv(r"/Users/Jack/Documents/Projects/Whatscooking-/input/df_parsed.csv", index=False)
 
-# for word, frequency in vocabulary.most_common(200):
-#     print(u'{};{}'.format(word, frequency))
-# fdist = nltk.FreqDist(ingredients)
+    # vocabulary = nltk.FreqDist()
+    # for ingredients in recipe_df['ingredients']:
+    #     ingredients = ingredients.split()
+    #     vocabulary.update(ingredients)
+            
+    # for word, frequency in vocabulary.most_common(200):
+    #     print(f'{word};{frequency}')
+    # fdist = nltk.FreqDist(ingredients)
 
-# common_words = []
-# for word, _ in vocabulary.most_common(250):
-#     common_words.append(word)
-
-# Can also get list of nouns from wordnet if needs be
-
+    # common_words = []
+    # for word, _ in vocabulary.most_common(250):
+    #     common_words.append(word)
+    # print(common_words)
